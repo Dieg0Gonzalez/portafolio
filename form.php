56637da9af0e38c963c063f003e400f7539c1159
Dieg0Gonzalez <diego21gonzalez08@gmail.com>
2024-07-16 05:38:01 -0600

    Add files via upload

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php
+
+$nombre = $_POST['name'];
+$telefono = $_POST['phone'];
+$email = $_POST['email'];
+$mensaje = $_POST['message'];
+
+//Como me va a llevar a mi el mensaje
+
+$mensaje .= "Nombre: " . $nombre . ",\r\n";
+$mensaje .= "Email: " . $email . ",\r\n";
+$mensaje .= "Telefono: " . $telefono . ",\r\n";
+$mensaje .= "Mensaje: " . $mensaje . ",\r\n";
+
+$destinatario = 'diego21gonzalez08@gmail.com';
+$asunto = 'Diegaceny';
+
+//Funcion mail
+// A quien le mando el mail
+//mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+
+
+//Redireccion al haber enviado el form
+header('Location:index.html');
+
+?>
